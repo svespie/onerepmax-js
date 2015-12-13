8033edc62a28abee06131eb9a9139a43c60418b8
Stan Vespie <svespie@outlook.com>
2015-12-13 01:23:07 -0500

    Have the basic "core" tests lined up but only applied to the all() method. Roughly 1/9th code coverage (pure estimation)?

--- tests/spec/OneRepMaxSpec.js
+++ tests/spec/OneRepMaxSpec.js
@@ -7,11 +7,124 @@
     //noinspection JSUnresolvedFunction
     describe("all()", function () {
       //noinspection JSUnresolvedFunction
+      it("When no arguments are passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all()).toEqual({});
+      });
+
+      //noinspection JSUnresolvedFunction
+      it("When null for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(null)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction,JSUnresolvedFunction
+      it("When undefined for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(undefined)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When '' for weight and no reps argument passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all('')).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When 'random string' for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all('random string')).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When true for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(true)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When false for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(false)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When an empty object for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all({})).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When an empty array for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all([])).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When a populated object for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all({weight: 135, reps: 10 })).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When a populated array for weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all([135, 10])).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When a valid weight and no reps argument is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(135)).toEqual({});
+      });
+
+      //noinspection JSUnresolvedFunction
+      it("When null for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(null, null)).toEqual({});
+      });
+        //noinspection JSUnresolvedFunction,JSUnresolvedFunction
+      it("When undefined for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(undefined, undefined)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When '' for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all('', '')).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When 'random string' for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all('random string', 'random string')).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When true for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(true, true)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When false for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all(false, false)).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When an empty object for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all({}, {})).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When an empty array for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all([], [])).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When a populated object for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all({weight: 135, reps: 10 }, {weight: 135, reps: 10 })).toEqual({});
+      });
+      //noinspection JSUnresolvedFunction
+      it("When a populated array for weight and reps is passed in, an empty object should be returned.", function () {
+        //noinspection JSUnresolvedFunction
+        expect(OneRepMax.all([135, 10], [135, 10])).toEqual({});
+      });
+
+      //noinspection JSUnresolvedFunction
       it("When null for weight is passed in, an empty object should be returned.", function () {
         //noinspection JSUnresolvedFunction
         expect(OneRepMax.all(null, 10)).toEqual({});
       });
-        //noinspection JSUnresolvedFunction,JSUnresolvedFunction
+      //noinspection JSUnresolvedFunction,JSUnresolvedFunction
       it("When undefined for weight is passed in, an empty object should be returned.", function () {
         //noinspection JSUnresolvedFunction
         expect(OneRepMax.all(undefined, 10)).toEqual({});
